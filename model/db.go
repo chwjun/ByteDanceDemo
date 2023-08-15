2ae6421b0565d0f14430c10883c2c21127f386cd
Wen Peitao <3014453996@qq.com>
2023-08-15 12:16:31 +0800

    修改数据库索引

--- model/db.go
+++ model/db.go
@@ -25,7 +25,7 @@ type User struct {
 // 视频表
 type Video struct {
 	BaseModel
-	AuthorID uint   `gorm:"index:not null;comment:视频作者id"`
+	AuthorID uint   `gorm:"index;not null;comment:视频作者id"`
 	Title    string `gorm:"type:varchar(191);not null;comment:视频标题"`
 	PlayURL  string `gorm:"type:varchar(191);not null;comment:视频播放地址"`
 	CoverURL string `gorm:"type:varchar(191);not null;comment:视频封面地址"`
@@ -34,25 +34,25 @@ type Video struct {
 // 评论表
 type Comment struct {
 	BaseModel
-	UserID     uint   `gorm:"index:not null;comment:发布评论的用户id"`
-	VideoID    uint   `gorm:"index:not null;comment:评论视频的id"`
+	UserID     uint   `gorm:"index;not null;comment:发布评论的用户id"`
+	VideoID    uint   `gorm:"index:idx_video_action;not null;comment:评论视频的id"`
 	Content    string `gorm:"type:varchar(191);not null;comment:评论的内容"`
-	ActionType int    `gorm:"type:ENUM('1', '2');not null;comment:评论行为，1表示已发布评论，2表示删除评论"`
+	ActionType int    `gorm:"index:idx_video_action;type:ENUM('1', '2');not null;comment:评论行为，1表示已发布评论，2表示删除评论"`
 }
 
 // 点赞表
 type Like struct {
 	BaseModel
-	UserID  uint `gorm:"index:not null;comment:点赞用户id"`
-	VideoID uint `gorm:"index:not null;comment:点赞视频id"`
-	Liked   int  `gorm:"not null;default:1;comment:默认1表示已点赞，0表示未点赞"`
+	UserID  uint `gorm:"index:idx_user_video_liked;not null;comment:点赞用户id"`
+	VideoID uint `gorm:"index:idx_user_video_liked;not null;comment:点赞视频id"`
+	Liked   int  `gorm:"index:idx_user_video_liked;not null;default:1;comment:默认1表示已点赞，0表示未点赞"`
 }
 
 // 消息表
 type Message struct {
 	BaseModel
-	SenderID   uint   `gorm:"index:not null;comment:发送message的user id"`
-	ReceiverID uint   `gorm:"index:not null;comment:接收message的user id"`
+	SenderID   uint   `gorm:"index;not null;comment:发送message的user id"`
+	ReceiverID uint   `gorm:"index;not null;comment:接收message的user id"`
 	Content    string `gorm:"type:varchar(191);not null;comment:消息内容"`
 	ActionType int    `gorm:"type:ENUM('1', '2');not null;comment:消息行为，1表示发送/2表示撤回"`
 }
@@ -60,7 +60,7 @@ type Message struct {
 // 关系表
 type Relation struct {
 	BaseModel
-	UserID      uint `gorm:"index:not null;comment:用户id"`
-	FollowingID uint `gorm:"index:not null;comment:user id关注的用户id"`
-	Followed    int  `gorm:"not null;default:0;comment:默认0表示未关注，1表示已关注"`
+	UserID      uint `gorm:"index:idx_user_following_followed;not null;comment:用户id"`
+	FollowingID uint `gorm:"index:idx_user_following_followed;not null;comment:user id关注的用户id"`
+	Followed    int  `gorm:"index:idx_user_following_followed;not null;default:0;comment:默认0表示未关注，1表示已关注"`
 }
