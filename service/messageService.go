090badc7416a8974d72e1192fd5d77a4c6830ddf
JasonWen1 <zwenab@connect.ust.hk>
2023-08-16 23:27:42 +0800

    Add messageService definition file

---
+++ service/messageService.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"github.com/RaymondCode/simple-demo/controller"
+	"time"
+)
+
+type MessageService interface {
+	//Send message function
+	SendMessage(userId int64, toUserId int64, content string) error
+
+	//Get chat history function
+	GetChatHistory(userId int64, toUserId int64, searchTime time.Time) ([]controller.Message, error)
+
+	//user part
+	GetLatestMessage(userId int64, selectedUserId int64) (controller.Message, error)
+}
